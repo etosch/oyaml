7b6e411c23a05e1e8c98f91b3f0438ea76856c30
Martin Keegan <martin@no.ucant.org>
2013-06-18 22:37:23 +0100

    add (c) boilerplate

--- lib_test/test_all.ml
+++ lib_test/test_all.ml
@@ -1,4 +1,13 @@
 
+(*
+  Oyaml, a YAML parser for OCaml
+
+  Copyright (C) 2013  Martin Keegan
+
+  This programme is free software; you may redistribute and/or modify
+  it under the terms of the Apache License v2.0
+*)
+
 open Yaml
 
 let dump_yaml = function
