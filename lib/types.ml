b98558b5f7912cd4f859a3e8fb9e0b23bf77e5ee
Martin Keegan <martin@no.ucant.org>
2013-06-18 22:40:03 +0100

    add (c) boilerplate

--- lib/types.ml
+++ lib/types.ml
@@ -1,4 +1,13 @@
 
+(*
+  Oyaml, a YAML parser for OCaml
+
+  Copyright (C) 2013  Martin Keegan
+
+  This programme is free software; you may redistribute and/or modify
+  it under the terms of the Apache License v2.0
+*)
+
 type node =
 	| Scalar of string
 	| Map of string
--- lib/yaml.ml
+++ lib/yaml.ml
@@ -1,3 +1,13 @@
+
+(*
+  Oyaml, a YAML parser for OCaml
+
+  Copyright (C) 2013  Martin Keegan
+
+  This programme is free software; you may redistribute and/or modify
+  it under the terms of the Apache License v2.0
+*)
+
 open Types
 
 type t = Types.node list
