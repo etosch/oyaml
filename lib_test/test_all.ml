0948f4c179360bc77ad35c47202f8dea86aee3d6
Martin Keegan <martin@no.ucant.org>
2013-06-18 22:36:07 +0100

    add another example

--- lib_test/test_all.ml
+++ lib_test/test_all.ml
@@ -4,7 +4,21 @@ open Yaml
 let dump_yaml = function
 	| _ -> "not supported"
 
+(* worst ocaml i've ever written *)
+let read_all_stdin () =
+	let buf = ref "" in
+	let ic = stdin in
+		(try while true 
+			do let line = input_line ic in
+				   buf := !buf ^ line
+			done
+		 with End_of_file ->	
+			 close_in ic);
+		!buf
+
+
 let () = 
-	let s = Yaml.parse "INVALID INPUT\n" in
+	let text = read_all_stdin () in
+	let s = Yaml.parse text in
 		print_endline "Dummy output:";
 		print_endline (dump_yaml s)
